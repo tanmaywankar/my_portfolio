0f5c4eabe8fcfac6ffc0c387e01b791b000e3b16
Tanmay Wankar <62742498+tanmaywankar@users.noreply.github.com>
2026-02-04 15:27:53 +0530

    added a lot of stuff

---
+++ app.js
@@ -0,0 +1,25 @@
+gsap.registerPlugin(ScrollTrigger);
+
+window.addEventListener('DOMContentLoaded', () => {
+    const radius = 40;
+    const circumference = 2 * Math.PI * radius; 
+
+
+    gsap.fromTo(".progress-stroke", 
+        { strokeDashoffset: circumference }, 
+        { 
+            strokeDashoffset: (i, target) => {
+                const percent = target.getAttribute('data-percent');
+                return circumference - (percent / 100) * circumference;
+            },
+            duration: 1.5,
+            ease: "power2.out",
+            stagger: 0.2, 
+            scrollTrigger: {
+                trigger: ".Skills", 
+                start: "top 90%",
+                toggleActions: "play none none none" 
+            }
+        }
+    );
+});
