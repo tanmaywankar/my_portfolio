8a9187dcc9dccb446bc79b9db2a36ef7bcd9125a
Tanmay Wankar <62742498+tanmaywankar@users.noreply.github.com>
2026-02-06 21:46:51 +0530

    added more animations and cv download button

--- app.js
+++ app.js
@@ -7,8 +7,53 @@ const mouseTl = gsap.timeline({
     delay: 2,
     onComplete: initHeroParallax
 });
-const scrollTl = gsap.timeline();
 
+
+gsap.from(".navbar",
+    {
+    delay:1,              
+    backgroundColor: "#2d2d2d00",
+    boxShadow: "0px 0px 0px rgba(0, 0, 0, 0.200)",
+    duration: 0.5,              
+    ease: "power2.out" ,
+     scrollTrigger: {
+    trigger: ".Bento_grid", 
+    start: "top 50%",       
+    toggleActions: "play reverse play reverse" 
+    }
+    }
+);
+
+const cards = gsap.utils.toArray(".Grid_card");
+
+cards.forEach((card) => {
+    const animation = gsap.to(card,{
+        scale:1.05,
+        duration:0.3,
+        paused: true,
+        reverse: true,
+        ease: "power1.inOut"
+    });
+    card.addEventListener("mouseenter", () => animation.play());
+    card.addEventListener("mouseleave", () => animation.reverse());
+});
+
+
+const skills = gsap.utils.toArray(".Skills, .Status_card");
+
+skills.forEach((skill) => {
+    const animation = gsap.to(skill,{
+        boxShadow: "2px 8px 10px rgba(0, 0, 0, 0.200)",
+        y:-10,
+        scale:1.05,
+        duration:0.2,
+        paused: true,
+        reverse: true,
+        ease: "power1.inOut"
+    });
+    skill.addEventListener("mouseenter", () => animation.play());
+    skill.addEventListener("mouseleave", () => animation.reverse());
+});
 
 heroTl.from(".heading", {
     y: 100,
@@ -56,8 +101,8 @@ function initHeroParallax() {
         });
 
         gsap.to(".float_icon", {
-            x: xPos * 30,
-            y: yPos * 30,
+            x: xPos * 50,
+            y: yPos * 50,
             duration: 0.6,
             ease: "power2.out"
         });
@@ -86,7 +131,7 @@ gsap.from(".grid_item3, .grid_item4, .Status_card, .grid_item5", {
     }
 });
 
-scrollTl.from(".Skills",
+gsap.from(".Skills",
     {
     delay:1.5,
     y: 50,              
@@ -121,3 +166,16 @@ gsap.fromTo(".progress-stroke",
 
         }
     );
+
+    gsap.from(".email, .Handle_cover", {
+    y: 50,              
+    opacity: 0,          
+    duration: 1,       
+    stagger: 0.3,       
+    ease: "power2.out",  
+    scrollTrigger: {
+        trigger: ".contacts", 
+        start: "top 80%",       
+        toggleActions: "play none none none" 
+    }
+});
